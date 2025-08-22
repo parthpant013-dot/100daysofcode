901ac792819e29779e53607f8524fe0781ac42a1
parthpant013-dot <parthpant013@gmail.com>
2025-08-22 17:36:16 +0530

    changed

---
+++ first__10_question/program1.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("Enter Two Numbers :");
+    scanf("%d %d",&a,&b);
+
+
+    printf("Sum of %d and %d is %d",a,b,a+b);
+
+    return 0;
+}
